30b578bb1b927fffbea3bb45bbb8e9fec51066dd
Deya <deya@Octopus.local>
2021-10-03 15:36:08 +0300

    updates

--- generate.py
+++ generate.py
@@ -3,17 +3,28 @@
 
 import os
 import json
-  
+import sys
+
 output_file = ""
 target_folder = ""
 include_types = []
 
-# Opening config file
-with open('config.json') as json_file:
-    data = json.load(json_file)
-    include_types = data['include_types']
-    output_file = data['output_file']
-    target_folder = data['target_folder']
+args = sys.argv[1:]
+
+if len(args) == 0:
+    # Opening config file
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        include_types = data['include_types']
+        output_file = data['output_file']
+        target_folder = data['target_folder']
+elif len(args) == 3:
+    include_types = args[0]
+    output_file = args[1]
+    target_folder = args[2]
+else:
+    print("invalid number of arguments.")
+    sys.exit()
 
 original_pwd = os.getcwd()
 
@@ -35,6 +46,9 @@ thisdir = os.getcwd()
 with open(output_file, 'w') as outfile:
     for names in filenames:
         with open(names) as infile:
-            outfile.write(infile.read())
-        print(f"writing {names}")
+            try:
+                outfile.write(infile.read())
+                print(f"scanned {names}")
+            except Exception as e:
+                print("skipped",names)
         outfile.write("\n")
