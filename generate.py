d38a1d139fd6b3ffa9e0184bdbd1a3435db549bd
Deya Eldeen Elkhawaldeh <3denator@gmail.com>
2021-10-03 11:59:20 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ import os
 import json
   
 output_file = ""
+target_folder = ""
 include_types = []
 
 # Opening config file
@@ -12,8 +13,11 @@ with open('config.json') as json_file:
     data = json.load(json_file)
     include_types = data['include_types']
     output_file = data['output_file']
+    target_folder = data['target_folder']
 
-# Getting the current work directory (cwd)
+original_pwd = os.getcwd()
+
+os.chdir(target_folder) #os.getcwd()
 thisdir = os.getcwd()
 
 # r=root, d=directories, f = files
@@ -23,6 +27,9 @@ for r, d, f in os.walk(thisdir):
         for ext in include_types:
             if file.endswith(ext):
                 filenames.append(os.path.join(r, file))
+
+os.chdir(original_pwd)
+thisdir = os.getcwd()
 
 # Open file3 in write mode
 with open(output_file, 'w') as outfile:
